364bd08d9ab5977f0fb6450a1e751b070591e1ae
Stan Jenkins <mc.stanislaw@gmail.com>
2021-01-29 01:51:46 +0000

    auto generated docs from Jenkins

---
+++ navtreedata.js
@@ -0,0 +1,125 @@
+var NAVTREE =
+[
+  [ "Stan Math Library", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", "namespacemembers_type" ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", "functions_type" ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ],
+    [ "Contributor Guide", "contributing.html", [
+      [ "Developer Guide", "developer_guide.html", null ],
+      [ "Testing Automatic Differentiation Functions", "autodiff_test_guide.html", null ],
+      [ "Testing New Distributions", "dist_tests.html", null ],
+      [ "Add New Functions With Known Gradients", "new_grad.html", null ],
+      [ "Adding New OpenCL Functions", "opencl_guide.html", null ],
+      [ "Windows Development Tips", "windows_tips.html", null ]
+    ] ],
+    [ "Parallelism", "usergroup0.html", [
+      [ "Threading", "tbb_threading.html", null ],
+      [ "OpenCL for GPU Computing", "opencl_support.html", null ],
+      [ "MPI Support", "mpi.html", null ]
+    ] ],
+    [ "(External Link) Stan Language Docs", "^https://mc-stan.org/users/documentation/", null ],
+    [ "(External Link) Stan Discourse", "^https://discourse.mc-stan.org/", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"d0/d20/grad__hessian_8hpp.html",
+"d0/d69/classstan_1_1math_1_1broadcast__.html#gac57dc36c31fd6ed33181f500f0d73f09",
+"d0/d8f/classstan_1_1math_1_1logical__or__.html#afb04ab4d13a748217f59e9f125abfd5f",
+"d0/dd2/prim_2fun_2is__uninitialized_8hpp.html",
+"d1/d08/classstan_1_1math_1_1acosh__.html#aa2db1f45f8cc85105ccc567d49f763b3",
+"d1/d64/group__var__dense__dynamic__types.html#gadc5ddab10a0847ae26c5b75ce65d5aa4",
+"d1/d91/group__eigen__types.html#gac5c09ee3b8c728f91aaa217ca6a46680",
+"d1/dbc/is__double__or__int_8hpp.html#gaab3157e31d62ac7e848cdd58d1b32221",
+"d2/d06/classstan_1_1math_1_1internal_1_1divide__dv__vari.html#a2442bb729d0afe617b6c24accae7d10b",
+"d2/d3c/group__opencl__kernel__generator.html#ga6629cc3da666cdfb08eb018ea2af7270",
+"d2/d5b/classstan_1_1math_1_1internal_1_1ops__partials__edge_3_01_dx_00_01std_1_1vector_3_01fvar_3_01_dx_01_4_01_4_01_4.html",
+"d2/d66/group__eigen__col__vector__types.html#ga95850b4b1cef160569f33fb322234d7e",
+"d2/d9d/opencl_2kernels_2categorical__logit__glm__lpmf_8hpp.html#ga7ac25b1a2e3a6110a014dcb0211f66db",
+"d2/dce/rev_2fun_2tanh_8hpp.html#ad087da016f682bc2ce1899d8d02389fe",
+"d3/d24/classstan_1_1math_1_1colwise__max__.html#af3931512c0883833dbc7c4361f0bfd6a",
+"d3/d6b/classstan_1_1math_1_1log1m__inv__logit__.html#a5ff2b5788fc0a2f570ef4818a2c7a15b",
+"d3/dcd/fwd_2fun_2inc__beta_8hpp.html",
+"d4/d33/err_2check__diagonal__zeros_8hpp.html",
+"d4/d7a/is__var__matrix_8hpp.html#ga876fa542eb94f37b098b0647b3e2397a",
+"d4/db1/group__eigen__array__types.html#ga567b1d98245a1c2953f5cffa1567d98c",
+"d4/dcc/std__normal__rng_8hpp.html#ga817cc9eafbbbea3ff44586ab9c54222e",
+"d5/d2f/wiener__lpdf_8hpp.html#gaccd61b4dd530c96e95da436b4465dd93",
+"d5/d5b/classstan_1_1math_1_1sqrt__.html#a61fad83b56aaff4720084d71958ab9cf",
+"d5/d89/rev_2fun_2initialize__fill_8hpp.html",
+"d5/db4/group__prob__dists.html#gab05d23263a6a6f24dabd3bc4ea9872ed",
+"d5/de5/group__opencl.html#ga0c8837ecfc67bc2624979c3633d76411",
+"d6/d0b/prim_2meta_2is__var_8hpp.html#gabbbcb9c67fd25de770393a77fe4c8c5b",
+"d6/d34/exp__mod__normal__ccdf__log_8hpp.html#ga13f85db1a8372d85153e8839f58dd66b",
+"d6/d7b/mpi__cluster__inst_8cpp.html",
+"d6/daf/autodiffstackstorage_8hpp_source.html",
+"d6/de6/for__each_8hpp.html#a277df299f921ffa3d595e2e7c374bda8",
+"d7/d40/_f32_8hpp_source.html",
+"d7/d8c/classstan_1_1math_1_1complex__base.html#a3e9fb7550f288d1c7e50916dd7f804ca",
+"d7/dc9/namespace_eigen_1_1internal.html#a81898bbc959510b08fb416bac927b818",
+"d8/d12/classstan_1_1math_1_1col__index.html",
+"d8/d5b/structstan_1_1math_1_1internal_1_1less__or__equal.html#a3f97a36c301a8be98eb1c07e3c1dbabb",
+"d8/dbf/classstan_1_1math_1_1gevv__vvv__vari.html#ab65e5affbfbfcaf260d76d1f06c87939",
+"d8/de1/group__type__trait.html#ga501ec44e68851a8396f5f693e35a2e69",
+"d9/d1f/classstan_1_1math_1_1internal_1_1hypot__vd__vari.html#a1b935420a058a69f8c57980df3ce74c4",
+"d9/d65/get__base1_8hpp.html#aca7c84b936da0d3422507927cf9a68cc",
+"d9/db7/eigenvalues_8hpp.html#a01b765a3e1a3af886b5a908a665dbaad",
+"d9/dfa/prim_2prob_2frechet__lpdf_8hpp.html#aa89998c5c945ded5581b03e80589635d",
+"da/d3e/accumulate__adjoints_8hpp.html",
+"da/d94/classstan_1_1math_1_1matrix__cl_3_01_t_00_01require__arithmetic__t_3_01_t_01_4_01_4.html#ab24b0c3fd027a1ba07e9f158bbc2ae84",
+"da/dc3/structstan_1_1math_1_1include__summand_3_01propto_00_01_t_01_4.html",
+"db/d1d/classstan_1_1math_1_1var__value_3_01_t_00_01require__floating__point__t_3_01_t_01_4_01_4.html#ab270e3c655c5b6b6da7f2fc733eb4976",
+"db/d62/group__vector__like__types.html#gaa9a320c13a88c10811f1bd21c42103e7",
+"db/df2/classstan_1_1math_1_1internal_1_1beta__dv__vari.html#aecde486589ff679c25496023c906035e",
+"dc/d37/fwd_2fun_2log__diff__exp_8hpp.html",
+"dc/d5d/group__opencl__kernels.html#gaf1e39dcc4fa296a0a94144f375fdaf9a",
+"dc/d88/is__vector_8hpp.html#gad0c785d1a217808d89e5f947e31abc60",
+"dc/de3/group__rev__matrix__types.html#ga4d2355dcaf17bc4e50dee95ab75a0f8f",
+"dd/d21/group__std__vector__types.html#ga446b1201f84ba0c1256d06398297bd05",
+"dd/d6c/rev_2fun_2divide_8hpp.html#a2811d0ad639cc2751d63285448fe0be2",
+"dd/dad/prim_2prob_2gumbel__lpdf_8hpp.html#gad2e25e81b8a0cc41cd0623d12394df66",
+"dd/deb/prim_2prob_2neg__binomial__2__log__lpmf_8hpp.html",
+"de/d28/ordered__free_8hpp_source.html",
+"de/d5f/group__opencl__context__group.html#gae215f1e6c88ed2690ae6c608689d5c11",
+"de/da0/int__step_8hpp.html#a1f65194aba95541f409d496c7ec90187",
+"de/de9/structstan_1_1math_1_1_scoped_chainable_stack_1_1activate__scope.html#a1366f69565a4447cf0675a1fe87051eb",
+"df/d4b/prim_2core_2operator__division_8hpp.html#ab044410dbdef305f48c6762de52b922b",
+"df/dbe/multi__student__t__log_8hpp.html#ga5ad01e45f4c8eabe5c813e0cc4eece98",
+"df/dfb/ordered__probit__rng_8hpp.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
